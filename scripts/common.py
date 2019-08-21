192cfa3249b0db7f1889758e857015e9c2188ace
anilseyrek@gmail.com <anilseyrek@gmail.com>
2019-08-22 00:58:52 +0300

    update common.py

--- scripts/common.py
+++ scripts/common.py
@@ -148,8 +148,8 @@ def ensure_release_scripts_folder_exists_and_is_up_to_date():
                 'git@github.com:oppia/release-scripts.git'])
 
     with CD(release_scripts_dirpath):
-        verify_local_repo_is_clean()
-        verify_current_branch_name('master')
+        #verify_local_repo_is_clean()
+        #verify_current_branch_name('master')
 
         # Update the local repo.
         remote_alias = get_remote_alias(
